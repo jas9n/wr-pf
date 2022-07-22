966dc0605172c0deb8cc25c50b31c48a03248930
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2022-07-22 19:16:54 -0400

    Update GlobalFooter.vue

--- portfolio/components/GlobalFooter.vue
+++ portfolio/components/GlobalFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="footer">
-        <div id="container" class="w-full h-64 flex flex-col justify-between items-center p-12 overflow-hidden md:flex-row sm:h-56 md:h-44 md:px-16 lg:px-20">
+        <div id="container" class="w-full h-52 flex flex-col justify-between items-center py-12 overflow-hidden md:flex-row md:h-44 md:px-16 lg:px-20">
             <div id="links" class="overflow-visible flex flex-col justify-center items-center md:items-start">
                 <a id="contact" href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=team@webrising.org" target="_blank">
                     <h5 class="text-white text-xl font-medium">team@webrising.org</h5>
@@ -9,14 +9,14 @@
                     <img src="@/assets/icons/github.svg" alt="" class="h-16 w-auto"/>
                 </a>
             </div>
-            <ul id="nav-list" class="flex justify-center items-center text-lg overflow-visible text-white">
-                <li class="relative table-cell"><nuxt-link to="/about" id="underline" class="relative inline-block pb-1">About Us</nuxt-link></li>
-                <li class="relative table-cell"><nuxt-link to="/projects" id="underline" class="relative inline-block pb-1">Our Work</nuxt-link></li>
-                <li class="relative table-cell"><nuxt-link to="/services" id="underline" class="relative inline-block pb-1">Our Services</nuxt-link></li>
-                <li class="relative table-cell"><a id="underline" href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=team@webrising.org" target="_blank" class="relative inline-block pb-1">Contact Us</a></li>
+            <ul id="nav-list" class="flex justify-center items-center text-base overflow-visible text-white space-x-6 sm:space-x-8 lg:space-x-12 sm:text-lg md:text-base lg:text-lg">
+                <li class="relative table-cell"><nuxt-link to="/about" id="underline" class="relative inline-block pb-1">About</nuxt-link></li>
+                <li class="relative table-cell"><nuxt-link to="/projects" id="underline" class="relative inline-block pb-1">Projects</nuxt-link></li>
+                <li class="relative table-cell"><nuxt-link to="/services" id="underline" class="relative inline-block pb-1">Services</nuxt-link></li>
+                <li class="relative table-cell"><a id="underline" href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=team@webrising.org" target="_blank" class="relative inline-block pb-1">Contact</a></li>
             </ul>
         </div>
-        <h6 id="copyright" class="text-center text-zinc-500 text-sm font-semibold mx-12 mb-8 lg:mx-20 lg:text-left">Copyright 2022 WebRising All Rights Reserved</h6>
+        <h6 id="copyright" class="text-center text-zinc-500 text-sm font-semibold md:mx-16 mb-8 lg:mx-20 md:text-left">Copyright 2022 WebRising All Rights Reserved</h6>
     </div>
 </template>
 
