a5f778aa8dbd09e8e22320b33d7b3481c7e5eb9e
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-10-02 18:50:49 -0400

    .

--- portfolio/components/Profile.vue
+++ portfolio/components/Profile.vue
@@ -1,22 +1,22 @@
 <template>
-    <div id="profile">
-        <div id="content" class="w-80 h-80 flex justify-center items-center cursor-pointer group border border-black rounded-sm sm:w-96 sm:h-96 md:w-80 md:h-80 lg:w-96 lg:h-96">
-            <div class="w-full h-full aspect-square object-cover overflow-hidden ease-in duration-200 group-hover:scale-110">
-                <img src="" alt="">
-            </div>
-            <div id="overlay" class="absolute w-80 h-80 flex flex-col justify-center z-10 items-center space-y-1 sm:w-96 sm:h-96 md:w-80 md:h-80 lg:w-96 lg:h-96">
-                <div class="absolute w-full h-full bg-black opacity-0 -z-10 ease-in duration-200 group-hover:opacity-60"></div>
-                <div class="text-white ease-in duration-200 opacity-0 group-hover:opacity-100">
-                    <h3 class="text-2xl font-medium">Team Member</h3>
-                    <h4 class="text-lg">Position</h4>
-                </div>
-            </div>
+  <div id="profile">
+    <div id="content" class="w-80 h-80 flex justify-center items-center cursor-pointer group border border-black rounded-sm sm:w-96 sm:h-96 md:w-80 md:h-80 lg:w-96 lg:h-96">
+      <div class="w-full h-full aspect-square object-cover overflow-hidden ease-in duration-200 group-hover:scale-110">
+        <img src="" alt="" />
+      </div>
+      <div id="overlay" class="absolute w-80 h-80 flex flex-col justify-center z-10 items-center space-y-1 sm:w-96 sm:h-96 md:w-80 md:h-80 lg:w-96 lg:h-96">
+        <div class="absolute w-full h-full bg-black opacity-0 -z-10 ease-in duration-200 group-hover:opacity-60"></div>
+        <div class="text-white ease-in duration-200 opacity-0 group-hover:opacity-100">
+          <h3 class="text-2xl font-medium">Team Member</h3>
+          <h4 class="text-lg">Position</h4>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "TeamProfile",
+  name: 'TeamProfile',
 }
 </script>
--- portfolio/pages/index.vue
+++ portfolio/pages/index.vue
@@ -3,13 +3,13 @@
     <global-navbar />
     <div id="birds" class="fixed inset-0 bg-black h-screen -z-20"></div>
     <div id="overlay" class="fixed inset-0 bg-black opacity-40 h-screen -z-10"></div>
-    <home-section>
-      <h2 class="text-6xl font-medium overflow-visible">We are WebRising.</h2>
-      <p class="text-xl">Built by high school developers — for you, for free.</p>
+    <home-section id="splash">
+      <h2 id="weare" class="text-6xl font-medium overflow-visible">We are WebRising.</h2>
+      <p id="built" class="text-xl">Built by high school developers — for you, for free.</p>
     </home-section>
     <home-section>
       <h2 class="text-6xl font-medium overflow-visible">Don't limit your potential.</h2>
-      <p class="text-xl">We have the resources and technical expertise to craft your streamlined platform.</p>
+      <p class="text-xl">We have the resources and technical expertise to craft streamlined platforms.</p>
     </home-section>
     <home-section>
       <h2 class="text-6xl font-medium overflow-visible">Tailored to your needs.</h2>
@@ -18,28 +18,28 @@
     <section id="projects" class="h-auto min-h-screen w-full flex justify-center items-center">
       <div id="content" class="w-full max-w-4xl flex flex-col justify-center items-center space-y-8 text-white text-center">
         <h2 class="text-6xl font-medium overflow-visible px-8">Check out our projects.</h2>
-      <div id="container" class="w-full flex flex-col justify-center items-center space-y-6">
-        <!-- <client/>
+        <div id="container" class="w-full flex flex-col justify-center items-center space-y-6">
+          <!-- <client/>
         <client/>
         <client/>
         <client/> -->
-      </div>
-      <button class="px-4 py-2 border border-white text-lg text-white rounded ease-in duration-200 hover:bg-white hover:text-black">
-        <p>More Work</p>
-      </button>
+        </div>
+        <nuxt-link to="/projects" class="px-4 py-2 border border-white text-lg text-white rounded ease-in duration-200 hover:bg-white hover:text-black">
+          <p>More Work</p>
+        </nuxt-link>
       </div>
     </section>
     <home-section>
       <h2 class="text-6xl font-medium overflow-visible">Get started.</h2>
       <!-- <p class="text-xl">Reach out to us to get started.</p> -->
-      <p class="text-xl">Let's create something<span class="text-rose-300"> awesome</span>!</p>
+      <p class="text-xl">Let's craft something<span class="text-rose-300"> beautiful</span>.</p>
       <a id="contact" href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=team@webrising.org" target="_blank">
         <button class="px-4 py-2 border border-white text-lg text-white rounded ease-in duration-200 hover:bg-white hover:text-black">
           <p>Contact Us</p>
         </button>
       </a>
     </home-section>
-    <global-footer/>
+    <global-footer />
   </div>
 </template>
 
@@ -50,14 +50,18 @@ import ScrollTrigger from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
-console.log(ScrollTrigger)
-
 export default {
   name: 'Home',
   data() {
     return {}
   },
   mounted() {
+    gsap
+      .timeline({ defaults: { duration: 1 } })
+      .from('#splash', { opacity: 0, ease: 'power3.in' }, 0)
+      .from('#weare', { y: -30, opacity: 0, ease: 'power2.out' }, 1)
+      .from('#built', { y: -20, opacity: 0, ease: 'power2.out' }, 2)
+
     this.vantaEffect = BIRDS({
       el: '#birds',
       mouseControls: false,
