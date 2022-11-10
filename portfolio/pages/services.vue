a752bd363910c9edd542f5b2cb58679a343a5a27
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2022-11-09 20:47:56 -0500

    fixed

--- portfolio/pages/services.vue
+++ portfolio/pages/services.vue
@@ -4,7 +4,7 @@
     <section class="min-h-screen w-full flex flex-col justify-center items-center pt-32 pb-12 lg:pt-4 md:space-y-6">
       <h2 class="text-white text-5xl font-medium text-center overflow-visible">What we offer.</h2>
       <div id="services" class="flex justify-center items-start flex-row flex-wrap gap-6 overflow-visible">
-        <service class="w-1/6" v-for="service in services" :key="service.name" :name="service.name" :logo="service.logo" :url="service.url" :description="service.description" />
+        <service v-for="service in services" :key="service.name" :name="service.name" :logo="service.logo" :url="service.url" :description="service.description" />
       </div>
     </section>
     <global-footer />
